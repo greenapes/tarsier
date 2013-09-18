3a39887e9137e3d0b7e01f6681b495584c1c7c08
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2013-09-18 11:49:26 +0200

    grunt add concat plugin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,19 +1,29 @@
 module.exports = function(grunt) {
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+			  separator: ';',
+			},
+			dist: {
+			  src: ['src/tarsier.js', 'src/ga_variables.js', 'src/ga_handlers.js'],
+			  dest: 'build/all.js',
+			}
+		},
 		uglify: {
 		  options: {
 		    // the banner is inserted at the top of the output
 		    banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
 		  },
 		  build: {
-	        src: 'src/<%= pkg.name %>.js',
+	        src: 'build/all.js',
 	        dest: 'build/<%= pkg.name %>.min.js'
 	      }
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.registerTask('default', ['uglify']);
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.registerTask('default', ['concat', 'uglify']);
 };
 
