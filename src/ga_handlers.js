19591e48d9877ee5c02ccb7bf1a936244a15fc19
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2013-09-24 13:26:46 +0200

    launch the animations or the timeout only after the widget is ready
    when the widget is ready it will send a postMessage to the parent with the index of his reference on the widgets array

--- src/ga_handlers.js
+++ src/ga_handlers.js
@@ -11,14 +11,29 @@ function info_embed(node){
     var url = "{1}/widget#/tribes-actions/{0.ape}/stats/monthly/2013/{0.month}/section/{0.topic}?preload=true&animation={0.animation}&id={2}".supplant([o, default_url, n]);
     iframe.src = url;
     node.appendChild(iframe);
-    var ms = parseFloat(o.animation);
-    if(!isNaN(ms)){ //mean that the parameter is a number that represent the delay in ms before we start the animation
-    	setTimeout(function(){
-    		T.startAnimation(node);
-    	}, ms)
-    }
     T.widgets.push(node);
 }
 
 /* functions declaration for various widget embedding */
 T.registerHandler("ga:info-tribe", info_embed);
+
+
+// 
+function receiveMessage(event)
+{
+    // we read the id from the parameter
+    if(event.data.msg == "ready!") {
+        var node = T.widgets[event.data.id];
+        var o = T.getOptions(node, ["ape", "month", "topic", "animation"]);
+        var ms = parseFloat(o.animation);
+        // if ms is != from Nan mean that the parameter is a number 
+        // that represent the delay in ms before we start the animation
+        if(!isNaN(ms)){
+            setTimeout(function(){
+                T.startAnimation(node);
+            }, ms)
+        }
+    }
+    
+}
+window.addEventListener("message", receiveMessage, false);
