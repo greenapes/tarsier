bf256e76d21728f75ea7ff3aa399f2a16a11d5bf
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2014-04-03 15:20:19 +0200

    info tribe "embedded" attribute to hide the footer

--- src/ga_handlers.js
+++ src/ga_handlers.js
@@ -1,8 +1,8 @@
 //var default_url = default_url || "https://fbapp.greenapes.com/";
 var default_url = default_url || "https://fb-greenapes.herokuapp.com/";
 
-function info_embed(node){
-    var o = T.getOptions(node, ["ape", "date", "duration", "topic", "animation", "preload"]);
+function info_tribe_embed(node){
+    var o = T.getOptions(node, ["ape", "date", "duration", "topic", "animation", "preload", "embedded"]);
     var iframe = document.createElement('iframe');
     iframe.style.width = "100%";
     iframe.style.height = "100%";
@@ -21,6 +21,33 @@ function info_embed(node){
     o["preload"] = o["preload"] || "fade";
     o["duration"] = o["duration"] || "";
     var url = "{1}/widget#/tribes-actions/{0.ape}/stats/interval/section/{0.topic}?start_date={0.date}&duration={0.duration}&preload=true&preload_animation={0.preload}&animation={0.animation}&id={2}".supplant([o, default_url, n]);
+    if(o["embedded"]) {
+        url += "&embedded";
+    }
+    iframe.src = url;
+
+    try{
+        node.appendChild(iframe);
+    }catch(e){
+    }
+
+    T.widgets.push(node);
+
+    function onReady(e) {
+        var node = e.target;
+        var o = T.getOptions(node, ["animation"]);
+        var ms = parseFloat(o.animation);
+        // if ms is != from Nan mean that the parameter is a number 
+        // that represent the delay in ms before we start the animation
+        if(!isNaN(ms)){
+            setTimeout(function(){
+                T.startAnimation(node);
+            }, ms);
+        }
+    }
+
+    T.addEventListener(node, 'loaded', onReady);
+}
     iframe.src = url;
 
     try{
@@ -47,7 +74,7 @@ function info_embed(node){
 }
 
 /* functions declaration for various widget embedding */
-T.registerHandler("ga:info-tribe", info_embed);
+T.registerHandler("ga:info-tribe", info_tribe_embed);
 var currentHandler;
 
 function receiveMessage(event)
